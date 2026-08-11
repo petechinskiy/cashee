d78bbf9d016b1ce843083674ae5c21097269a51a
nikolay.nagornov <nagornov.na@gmail.com>
2026-08-11 12:16:05 +0300

    upd

--- Assets/Scripts/AdjoeController.cs
+++ Assets/Scripts/AdjoeController.cs
@@ -9,40 +9,71 @@ public class AdjoeController : MonoBehaviour
     private const string SDK_HASH = "d0013f0948e21cef3f0be6de36f7d90c";
 
     public Action _onOfferwallShow;
+    private PlaytimeOptions _playtimeOptions;
 
     public void Init(string userId, string network, string channel, string subpublisher, Action onOfferwallShow)
     {
-        var playtimeParams = new PlaytimeParams
-        {
-            uaNetwork = network,
-            uaChannel = channel,
-            uaSubPublisherEncrypted = subpublisher,
-        };
+        var options = new PlaytimeOptions();
 
-        var options = new PlaytimeOptions()
-        {
-            userId = userId,
-            playtimeParams = playtimeParams
-        };
+        PlaytimeParams adjoeParams = new PlaytimeParams();
+        adjoeParams.SetUaNetwork(network)
+                .SetUaChannel(channel)
+                .SetUaSubPublisherCleartext(subpublisher);
 
-        _onOfferwallShow = onOfferwallShow;
+        options.SetPlaytimeParams(adjoeParams);
 
-        Playtime.Init(SDK_HASH, options, AdjoeInitialisationSuccess, AdjoeInitialisationError);
-    }
+        var userProfile = new PlaytimeUserProfile();
+        //userProfile.SetGender(PlaytimeGender.FEMALE)
+        //        .SetBirthday(DateTime.Parse("1990-05-15T00:00:00.000Z"));
 
-    public void AdjoeInitialisationSuccess()
-    {
-        Debug.Log("Adjoe initialization successful.");
-    }
+        //options.SetPlaytimeUserProfile(userProfile);
 
-    public void AdjoeInitialisationError(Exception exception)
-    {
-        Debug.LogError($"Adjoe initializing is fault: {exception.Message}");
+        options.SetUserId(userId);
+        options.SetSdkHash(SDK_HASH);
+
+        _playtimeOptions = options;
+
+        Playtime.SetPlaytimeOptions(
+            options,
+            () =>
+            {
+                Playtime.GetStatus((status) =>
+                {
+                    Debug.Log($"ADJOE - Current SDK Status: {status}");
+
+                    if (status != null && status.IsInitialized)
+                    {
+                        // Step 2: Show the Playtime catalog
+                    }
+                    else
+                    {
+                        Debug.LogWarning("ADJOE - SDK is not initialized yet. Please try again in a moment.");
+                    }
+                }, (error) =>
+                {
+                    Debug.LogWarning($"Adjoe status error: {error.Message}");
+                });
+            },
+            error =>
+            {
+                Debug.LogError($"ADJOE - SetPlaytimeOptions Error: {error.Message}");
+            }
+        );
     }
 
     public void ShowOfferwall()
     {
-        Playtime.ShowCatalog();
+        Playtime.ShowCatalogWithOptions(
+            _playtimeOptions,
+            () =>
+            {
+                Debug.Log("ShowCatalogWithOptions Success!");
+            },
+            error =>
+            {
+                Debug.LogError($"ShowCatalogWithOptions Error: {error.Message}");
+            }
+        );
 
         _onOfferwallShow?.Invoke();
     }
